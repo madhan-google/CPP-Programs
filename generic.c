d90891e928b1bc690ec32b49d5917d2fe8ececc5
madhan-google <madhang6600@gmail.com>
2021-12-04 09:59:13 +0530

    updated

--- generic.c
+++ generic.c
@@ -3,11 +3,13 @@
 #include<stdlib.h>
 #define sf scanf
 #define pf printf
-#define pfn(n) printf("%d", n);
-#define pff(n) printf("%f", n);
-#define pfd(n) printf("%lf", n);
-#define pfc(ch) printf("%c", ch);
-#define pfs(s) printf("%s", s);
+#define pb push_back
+#define pfn(n) printf("%d ", n);
+#define ln() printf("\n");
+#define pff(n) printf("%f ", n);
+#define pfd(n) printf("%lf ", n);
+#define pfc(ch) printf("%c ", ch);
+#define pfs(s) printf("%s ", s);
 #define fl float
 #define dd double
 #define max(a, b) a>b?a:b
@@ -15,9 +17,15 @@
 #define FOR(i, s, e, c) for(i=(s);i<(e); i+=(c))
 int main()
 {
-	int n;
+	char str[10][10] = {"lskjdf", "slkdfj", "sldkjf"};
+	int n, i, j;
 	sf("%d", &n);
-	pfs(((n*n)%10==n)?"Automorphic Number":"Not Automorphic Number");
+	FOR(i, 0, n, 1){
+		FOR(j, 0, n, 1){
+			pfn(i+j)
+		}
+		ln();
+	}
 	return 0;
 }
 
