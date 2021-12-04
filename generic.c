e55e7b8fd7f4e9c9707c0047775df647135f9424
madhan-google <madhang6600@gmail.com>
2021-12-04 10:08:58 +0530

    updated

--- generic.c
+++ generic.c
@@ -5,26 +5,27 @@
 #define pf printf
 #define pb push_back
 #define pfn(n) printf("%d ", n);
-#define ln() printf("\n");
+#define ln printf("\n");
 #define pff(n) printf("%f ", n);
 #define pfd(n) printf("%lf ", n);
 #define pfc(ch) printf("%c ", ch);
 #define pfs(s) printf("%s ", s);
 #define fl float
 #define dd double
+#define sz(x) (int)x.size();
 #define max(a, b) a>b?a:b
 #define min(a, b) a>b?b:a
 #define FOR(i, s, e, c) for(i=(s);i<(e); i+=(c))
+//#define EACH(x, ar) for(auto& (x) : (ar))
 int main()
 {
 	char str[10][10] = {"lskjdf", "slkdfj", "sldkjf"};
 	int n, i, j;
 	sf("%d", &n);
-	FOR(i, 0, n, 1){
-		FOR(j, 0, n, 1){
-			pfn(i+j)
-		}
-		ln();
+	int ar[n];
+	FOR(i, 1, 6, 1){
+		pfn(i);
+		ln;
 	}
 	return 0;
 }
