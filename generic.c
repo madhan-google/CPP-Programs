293437b6507ebb5f25dc336ae6a460e91e6c6933
madhan-google <madhang6600@gmail.com>
2021-12-09 10:09:38 +0530

    generic update

--- generic.c
+++ generic.c
@@ -1,20 +1,21 @@
 #include<stdio.h>
 #include<math.h>
 #include<stdlib.h>
+#include<string.h>
 #define sf scanf
 #define pf printf
 #define nl pfs("\n");
 #define VD void
 #define pb push_back
-#define pfn(n) printf("%d ", n);
-#define pff(n) printf("%f ", n);
-#define pfd(n) printf("%lf ", n);
-#define pfc(ch) printf("%c ", ch);
-#define pfs(s) printf("%s ", s);
+#define pfn(n) printf("%d", n);
+#define pff(n) printf("%f", n);
+#define pfd(n) printf("%lf", n);
+#define pfc(ch) printf("%c", ch);
+#define pfs(s) printf("%s", s);
 #define sff(f) sf("%f", &f);
-#define sfn(f) sf("%d", &n);
+#define sfn(n) sf("%d", &n);
 #define sfc(ch) sf("%c", &ch);
-#define sfs(s) sf("%s", &s);
+#define sfs(s) gets(s);
 #define fl float
 #define dd double
 #define sz(x) (int)x.size();
@@ -22,33 +23,37 @@
 #define min(a, b) a>b?b:a
 #define INF while(1)
 #define WHL(i, n) while(i<n)
+#define WHL while
 #define FOR(i, s, e, c) for(i=(s);i<(e); i+=(c))
 #define BR break;
 #define CNT continue;
 #define _ pfs(" ");
+#define __ pfs("\t");
 #define PER(n, p) (n*(p/100.0))
+#define or ||
+#define and &&
+#define is ==
+#define isnot !=
+#define $ auto
 //#define EACH(x, ar) for(auto& (x) : (ar))
 int main()
 {
-	char ch;
-	sfc(ch)
-	if(ch>=65&&ch<=122){
-		if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U'){
-			pfs("vowel")
-		}else{
-			pfs("consonent")
-		}
-	}else{
-		pfs("not an alphabet")
-	}
+	
 }
-//int n;
-//	fl time;
-//	sfn(n)
-//	sff(time)
-//	if(time==2){
-//		pff(PER(time, 50)+time)
-//	}else{
-//		pff(time*time)
-//	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
